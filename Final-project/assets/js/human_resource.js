cd28af3a28151ff6a659d90cade79f8cbc6fc9aa
NigarMahmudova <nigarem@code.edu.az>
2023-04-29 06:39:36 -0700

    Feat: added hr.html, hr.scss, hr.js

---
+++ Final-project/assets/js/human_resource.js
@@ -0,0 +1,24 @@
+let buttons = document.querySelectorAll('.locations-links .locations-item');
+
+for(let btn of buttons){
+    btn.onclick = function(e) {
+        e.preventDefault();
+        let active = document.querySelector('.loc-active');
+
+        active.classList.remove('loc-active');
+        this.classList.add('loc-active');
+
+
+        let data_id = this.getAttribute('data-id');
+        let contents = document.querySelectorAll('.hr-contents .hr-content');
+
+        for(let content of contents){
+            if(content.id === data_id){
+                content.classList.remove('d-none');
+            }
+            else{
+                content.classList.add('d-none');
+            }
+        }
+    }
+}
